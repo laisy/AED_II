87363582f96f90af4b3eec79111bdbd606528a04
laisy <laisyferreira2011@gmail.com>
2020-08-23 20:15:35 -0300

    remover não tá funcionando direito não

--- Arvore/BST/main.c
+++ Arvore/BST/main.c
@@ -12,26 +12,40 @@ void main(){
     int opcao;
     do {
         scanf("%d", &opcao);
+        int chave;
         switch(opcao){
-            int chave;
             case 1:
                 scanf("%d", &chave);
                 raiz = inserir(raiz, chave);
                 break;
 
             case 2:
-                //printf("\nPre order: \n");
+                printf("\nPre order: \n");
                 preOrder(raiz);
+                printf("\n");
                 break;
 
             case 3:
-                //printf("\nIn order: \n");
+                printf("\nIn order: \n");
                 inOrder(raiz);
+                printf("\n");
                 break;
 
             case 4:
-                //printf("\nPos order: \n");
+                printf("\nPos order: \n");
                 posOrder(raiz);
+                printf("\n");
+                break;
+
+            case 5:
+                scanf("%d", &chave);
+                raiz = remover(raiz, chave);
+                break;
+
+            case 6:
+                printf("\nMaior elemento: \n");
+                printf("%d", maior(raiz));
+                printf("\n");
                 break;
 
             default:
